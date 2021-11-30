384fe509b5fc8fb73b09615875f9fb871946e038
FrancescoAndreotti <francescoandreotti00@gmail.com>
2021-11-30 17:15:40 +0100

    Added style.css

--- index.php
+++ index.php
@@ -60,16 +60,38 @@ $faq = [
 ];
 
 
-foreach ($faq as $sezione) {
-  $domanda = $sezione["domanda"];
-  $paragrafo = $sezione["paragrafi"];
-  echo $domanda;
-  foreach ($paragrafo as $s) {
-    echo $s;
-  }
-}
+
 
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="container">
+    <?php
+
+    foreach ($faq as $sezione) {
+      $domanda = $sezione["domanda"];
+      $paragrafo = $sezione["paragrafi"];
+      echo $domanda;
+      foreach ($paragrafo as $s) {
+        echo $s;
+      }
+    }
+
+    ?>
+  </div>
+</body>
 
 </html>
